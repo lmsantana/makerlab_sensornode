ed37e3ebbab80ab681069186d6f3c7b05721e0ef
Lucas Santana <lmsantana129@gmail.com>
2016-05-31 22:09:11 +0000

    Up to 4 node working

--- central_blue.py
+++ central_blue.py
@@ -7,39 +7,41 @@ import sys
 # BluePy Library Notes
 ##############################################
 
-#1 - Cannot manage to use getCharacteristicsbyUUID() method for UART,
-#	 so I hard code the source which is position [0] of characteristic vector
+# 1 - Cannot manage to use getCharacteristicsbyUUID() method for UART,
+#     so I hard code the source which is position [0] of characteristic vector
 
 ##############################################
 # TODO list
 ##############################################
 
-#TODO: change timeout to 10.0 after tests
-#TODO: graphical interface for inputting MAC address and input
-#TODO: use just dictionaries when user input the MAC address
-#TODO: Check Notifications support on Adafruit board to set delegates and receive
+# TODO: change timeout to 10.0 after tests
+# TODO: put data colelction of nodes in paralell mode (Thread) to improve the 2
+#       points per second to 10 points per second (node threshold)
+# TODO: graphical interface for inputting MAC address and input
+# TODO: use just dictionaries when user input the MAC address
+# TODO: Check Notifications support on Adafruit board to set delegates and receive
 #      notifications assynchronously
 
 ##############################################
 # Global values for Bluetooth
 ##############################################
 
-#Using for general storage
+# Using for general storage
 SPIRO_MAC_ADDR = 'd0:4f:8f:c9:d0:57'
 BNDSW_MAC_ADDR = 'ec:5b:e7:e0:96:c2'
 DRILL_MAC_ADDR = 'df:c5:a5:de:ee:9a'
 MILLI_MAC_ADDR = 'c6:73:0d:c1:66:ba'
 CIRCS_MAC_ADDR = 'ca:58:f6:d6:2f:ae'
 
-#Usign for friendly programming
+# Using for friendly programming
 MAC_DIC = {'BENDSAW':BNDSW_MAC_ADDR, 'DRILL_PRESS':DRILL_MAC_ADDR,
 			'MILL':MILLI_MAC_ADDR, 'CIRCULAR_SAW':CIRCS_MAC_ADDR,
 			'SPIROMETER':SPIRO_MAC_ADDR}
 
-#UUID for the Bluetooth UART Service based on Nordic Semiconductors Chip
+# UUID for the Bluetooth UART Service based on Nordic Semiconductors Chip
 UART_UUID = UUID('6E400001-B5A3-F393-E0A9-E50E24DCCA9E')
 
-#Global dictionary to hold Peripheral objects to nodes
+# Global dictionary to hold Peripheral objects to nodes
 nodes = {}
 
 ##############################################
@@ -54,13 +56,13 @@ dataz = ""
 ##############################################
 class ScanDelegate(DefaultDelegate):
     def __inti__(self):
-	DefaultDelegate.__init__(self)
+        DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-	if isNewDev:
-        print "Discovered device", dev.addr
-	elif isNewData:
-        print "Received new data from", dev.addr
+        if isNewDev:
+            print "Discovered device", dev.addr
+        elif isNewData:
+            print "Received new data from", dev.addr
 
 ###############################################
 # General Functions
@@ -78,125 +80,131 @@ def split_accel_data(data):
 ###############################################
 # Scanning Devices
 ###############################################
+os.system("sudo hciconfig hci0 down")
+os.system("sudo hciconfig hci0 up")
 print " "
 print "Scanning devices..."
-print " "
 scanner = Scanner().withDelegate(ScanDelegate())
-devices = scanner.scan(6.0)
+devices = scanner.scan(8.0)
 
 ###############################################
 # Connecting Devices
 ###############################################
 print " "
 print "Connecting to nodes..."
-print " "
 for dev in devices:
-    #Creating Bendsaw Peripheral
-	if dev.addr == MAC_DIC['BENDSAW']:
-	    print "Device %s (%s) Bendsaw found, connecting..." %(dev.addr, dev.addrType)
-	    bndsw = Peripheral(dev.addr, dev.addrType)
-		nodes['bendsaw'] = bndsw
-	    for (adtype, desc, value) in dev.getScanData():
+    # Creating Bendsaw Peripheral
+    if dev.addr == MAC_DIC['BENDSAW']:
+        print " "
+        print "Device %s (%s) Bendsaw found, connecting..." %(dev.addr,
+                                                              dev.addrType)
+        bndsw = Peripheral(dev.addr, dev.addrType)
+        nodes['bendsaw'] = bndsw
+        for (adtype, desc, value) in dev.getScanData():
             print "    %s = %s" % (desc, value)
-	    print " "
-	
-	#Creating Drilling Machine Peripheral
-	if dev.addr == MAC_DIC['DRILL_PRESS']:
-	    print "Device %s (%s) Bendsaw found, connecting..." %(dev.addr, dev.addrType)
-	    drill = Peripheral(dev.addr, dev.addrType)
-		nodes['drill_press'] = drill
-	    for (adtype, desc, value) in dev.getScanData():
-            print "    %s = %s" % (desc, value)
-	    print " "
-	
-	#Creating Circular Saw Peripheral
-	if dev.addr == MAC_DIC['CIRCULAR_SAW']:
-	    print "Device %s (%s) Bendsaw found, connecting..." %(dev.addr, dev.addrType)
-	    circs = Peripheral(dev.addr, dev.addrType)
-		nodes['circ_saw'] = circs
-	    for (adtype, desc, value) in dev.getScanData():
-            print "    %s = %s" % (desc, value)
-	    print " "
-		
-	#Creating Milling Machine Peripheral
-	if dev.addr == MAC_DIC['MILL']:
-	    print "Device %s (%s) Bendsaw found, connecting..." %(dev.addr, dev.addrType)
-	    milli = Peripheral(dev.addr, dev.addrType)
-		nodes['milling_machine'] = milli
-	    for (adtype, desc, value) in dev.getScanData():
-            print "    %s = %s" % (desc, value)
-	    print " "
-	
-	#Creating Spirometer Peripheral
-    if dev.addr == MAC_DIC['SPIROMENTER']:
-	    print "Device %s (%s) Spirometer found, connecting..." %(dev.addr, dev.addrType)
-	    spiro = Peripheral(dev.addr, dev.addrType)
-		nodes['spirometer'] = spiro
-	    for (adtype, desc, value) in dev.getScanData():
-            print "    %s = %s" % (desc, value)
-	    print " "
-		
 
-#Debugging loop
-#while 1:
+	# Creating Drilling Machine Peripheral
+    if dev.addr == MAC_DIC['DRILL_PRESS']:
+        print " "
+        print "Device %s (%s) Drilling Machine found, connecting..." %(dev.addr,
+                                                              dev.addrType)
+        drill = Peripheral(dev.addr, dev.addrType)
+        nodes['drill_press'] = drill
+        for (adtype, desc, value) in dev.getScanData():
+            print "    %s = %s" % (desc, value)
+
+	# Creating Circular Saw Peripheral
+    if dev.addr == MAC_DIC['CIRCULAR_SAW']:
+        print " "
+        print "Device %s (%s) Circular Saw found, connecting..." %(dev.addr,
+                                                              dev.addrType)
+        circs = Peripheral(dev.addr, dev.addrType)
+        nodes['circ_saw'] = circs
+        for (adtype, desc, value) in dev.getScanData():
+            print "    %s = %s" % (desc, value)
+
+	# Creating Milling Machine Peripheral
+    if dev.addr == MAC_DIC['MILL']:
+        print " "
+        print "Device %s (%s) Milling Machine found, connecting..." %(dev.addr,
+                                                              dev.addrType)
+        milli = Peripheral(dev.addr, dev.addrType)
+        nodes['milling_machine'] = milli
+        for (adtype, desc, value) in dev.getScanData():
+            print "    %s = %s" % (desc, value)
+
+	# Creating Spirometer Peripheral
+    if dev.addr == MAC_DIC['SPIROMETER']:
+        print " "
+        print "Device %s (%s) Spirometer found, connecting..." %(dev.addr,
+                                                                 dev.addrType)
+        spiro = Peripheral(dev.addr, dev.addrType)
+        nodes['spirometer'] = spiro
+        for (adtype, desc, value) in dev.getScanData():
+            print "    %s = %s" % (desc, value)
+
+# Debugging loop
+# while 1:
 #    print "BNDSW %s  || SPIRO %s" % (bndsw.getServiceByUUID(UART_UUID).getCharacteristics()[0].read(),
 #				     spiro.getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
-#end
+# end
 
-#Writing to doc loop
-#delete old file for testing
+###############################################
+# Main loop routine
+###############################################
+# Delete old file for testing
 os.system("sudo rm data/data_from_nodes.csv")
 
 file = open("data/data_from_nodes.csv", "a")
 while 1:
     try:
-		#Handling Bendsaw collection of data
+		# Handling Bendsaw collection of data
 		if nodes.has_key('bendsaw') is True:
 			data = str(nodes['bendsaw'].getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
 			if (data[0]=='X') and ('Y' in data) and ('Z' in data):
 				split_accel_data(data)
 				file.write("N1, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " +datax +", " +datay +", " +dataz)
-				
-		#Handling Drilling Machine collection of data
+
+		# Handling Drilling Machine collection of data
 		if nodes.has_key('drill_press') is True:
 			data = str(nodes['drill_press'].getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
 			if (data[0]=='X') and ('Y' in data) and ('Z' in data):
 				split_accel_data(data)
 				file.write("N2, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " +datax +", " +datay +", " +dataz)
-				
-		#Handling Milling Machine collection of data
+
+		# Handling Milling Machine collection of data
 		if nodes.has_key('milling_machine') is True:
 			data = str(nodes['milling_machine'].getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
 			if (data[0]=='X') and ('Y' in data) and ('Z' in data):
 				split_accel_data(data)
 				file.write("N3, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " +datax +", " +datay +", " +dataz)
-				
-		#Handling Circular Saw collection of data
+
+		# Handling Circular Saw collection of data
 		if nodes.has_key('circ_saw') is True:
 			data = str(nodes['circ_saw'].getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
 			if (data[0]=='X') and ('Y' in data) and ('Z' in data):
 				split_accel_data(data)
 				file.write("N4, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " +datax +", " +datay +", " +dataz)
-				
-		#Handling Spirometer collection of data
+
+		# Handling Spirometer collection of data
 		if nodes.has_key('spirometer') is True:
 			data = str(nodes['spirometer'].getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
 			file.write("N5, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " + data)
-        
-		
-		
-		#bndsw_data = str(bndsw.getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
-        #spiro_data = str(spiro.getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
-		
 
-	    #Drop bad packets data from bendsaw sensor
-	    #if (bndsw_data[0]=='X') and ('Y' in bndsw_data) and ('Z' in bndsw_data):
+
+
+		# bndsw_data = str(bndsw.getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
+        # spiro_data = str(spiro.getServiceByUUID(UART_UUID).getCharacteristics()[0].read())
+
+
+	    # Drop bad packets data from bendsaw sensor
+	    # if (bndsw_data[0]=='X') and ('Y' in bndsw_data) and ('Z' in bndsw_data):
         #    split_accel_data(bndsw_data)
         #    file.write("N1, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " +datax +", " +datay +", " +dataz)
-	    #file.write("N2, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " + str(spiro_data))
+	    # file.write("N2, " + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + ", " + str(spiro_data))
 
-	    print "."
     except KeyboardInterrupt:
-	    print "Python script was killed, closing file..."
-	    file.close()
-	    sys.exit()
+        print " "
+        print "Python script was killed, closing file..."
+        file.close()
+        sys.exit()
